651dec00ad2ad35b941a598ac0a1ea0d9a23a87d
ameliapham <ptt.huong.8895@gmail.com>
2024-06-05 01:22:10 +0200

    Update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,35 @@
+import { GlobalStyles } from "tss-react";
+import { Slide } from "./Slide";
+import { tss } from "tss-react/mui";
+
 export function App() {
-  return <h1>Hello World</h1>
+
+  const { classes } = useStyles();
+
+  return (
+    <>
+      <GlobalStyles styles={{
+        "*": {
+          "margin": 0,
+          "padding": 0,
+        },
+        "html, body": {
+          "backgroundColor": "black",
+        },
+      }}
+      />
+      <div className={classes.root}>
+        <Slide />
+      </div>
+    </>
+  )
 }
+
+const useStyles = tss.create(() => ({
+  "root": {
+    "width": "100vw",
+    "height": "100vh",
+    "overflow": "hidden",
+  },
+
+}));
