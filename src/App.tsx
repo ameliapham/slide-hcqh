c7c4ee31bda75c5af9e7357541f0b939ac68c26d
ameliapham <ptt.huong.8895@gmail.com>
2024-06-06 16:56:18 +0200

    Add fullscreen Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
 import { GlobalStyles } from "tss-react";
 import { Slide } from "./Slide";
 import { tss } from "tss-react/mui";
+import { FullscreenProvider } from "hooks/FullscreenContext";
 
 export function App() {
-
   const { classes } = useStyles();
 
   return (
-    <>
-      <GlobalStyles styles={{
-        "*": {
-          "margin": 0,
-          "padding": 0,
-        },
-        "html, body": {
-          "backgroundColor": "black",
-        },
-      }}
+    <FullscreenProvider>
+      <GlobalStyles
+        styles={{
+          "*": {
+            margin: 0,
+            padding: 0,
+          },
+          "html, body": {
+            backgroundColor: "black",
+          },
+        }}
       />
       <div className={classes.root}>
         <Slide />
       </div>
-    </>
-  )
+    </FullscreenProvider>
+  );
 }
 
 const useStyles = tss.create(() => ({
-  "root": {
-    "width": "100vw",
-    "height": "100vh",
-    "overflow": "hidden",
+  root: {
+    width: "100vw",
+    height: "100vh",
+    overflow: "hidden",
   },
-
 }));
--- src/Slide.tsx
+++ src/Slide.tsx
@@ -3,9 +3,12 @@ import { useState } from 'react';
 import IconButton from '@mui/material/IconButton';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import FullscreenIcon from '@mui/icons-material/Fullscreen';
+import FullscreenExitIcon from '@mui/icons-material/FullscreenExit';
 import ReactPlayer from 'react-player'
 import { alpha } from '@mui/material/styles';
 import { slides } from "data/dataSlides";
+import { useFullscreen } from 'hooks/FullscreenContext';
 
 
 type SlideProps = {
@@ -15,9 +18,9 @@ type SlideProps = {
 export function Slide(props: SlideProps) {
     const { className } = props;
     const [currentSlide, setCurrentSlide] = useState(0);
-
     const slide = slides[currentSlide];
     const { cx, classes } = useStyles();
+    const { enterFullscreen, isFullscreen, exitFullscreen } = useFullscreen();
 
     const goToNextImage = () => {
         setCurrentSlide((prevIndex) => (prevIndex + 1) % slides.length);
@@ -27,37 +30,49 @@ export function Slide(props: SlideProps) {
         setCurrentSlide((prevIndex) => (prevIndex - 1 + slides.length) % slides.length);
     }
 
+    const handleFullscreenToggle = () => {
+        if (isFullscreen) {
+            exitFullscreen();
+        } else {
+            enterFullscreen();
+        }
+    };
+
     return (
         <>
             <div className={cx(classes.root, className)}>
-                    <div className={cx(classes.image)}>
-                        {(() => {
-                            switch (slide.type) {
-                                case 'image':
-                                    return (
-                                        <img
-                                            src={slide.src}
-                                            alt={slide.alt}
-                                            className={cx(classes.image)}
-                                        />
-                                    );
-                                case 'video':
-                                    return (
-                                        <ReactPlayer
-                                            url={slide.src}
-                                            controls
-                                            width="100%"
-                                            height="100%"
-                                        />
-                                    );
-                                case 'component':
-                                    return (
-                                        <slide.Component />
-                                    );
-                            }
-                        })()}
-                    </div>
-             
+
+                <div className={cx(classes.image)}>
+                    {(() => {
+                        switch (slide.type) {
+                            case 'image':
+                                return (
+                                    <img
+                                        src={slide.src}
+                                        alt={slide.alt}
+                                        className={cx(classes.image)}
+                                    />
+                                );
+                            case 'video':
+                                return (
+                                    <ReactPlayer
+                                        url={slide.src}
+                                        controls
+                                        width="100%"
+                                        height="100%"
+                                    />
+                                );
+                            case 'component':
+                                return (
+                                    <slide.Component />
+                                );
+                        }
+                    })()}
+                </div>
+
+                <IconButton onClick={handleFullscreenToggle} className={classes.fullscreen}>
+                    {isFullscreen ? <FullscreenExitIcon /> : <FullscreenIcon />}
+                </IconButton>
 
                 <IconButton
                     className={cx(classes.button)}
@@ -85,9 +100,6 @@ const useStyles = tss
         return {
             "root": {
                 "position": "relative",
-                "display": "flex",
-                "alignContent": "center",
-                "justifyContent": "center",
                 "width": "100%",
                 "height": "100%",
                 "overflow": "hidden",
@@ -98,12 +110,12 @@ const useStyles = tss
                 "objectFit": "cover",
                 "objectPosition": "center center",
                 "background": "center center/cover",
-                "transition": "opacity .4s ease-in-out",
+                "transition": "opacity .5s ease-in-out",
             },
             "button": {
                 "position": "absolute",
                 "top": "0",
-                "justifyContent": "center",
+                //"justifyContent": "center",
                 "width": "150px",
                 "height": "80%",
                 "padding": "0",
@@ -112,14 +124,18 @@ const useStyles = tss
 
                 "&:first-of-type": {
                     "left": 0,
+                    "color": alpha(theme.palette.secondary.light, 0.01),
                 },
                 "&:last-of-type": {
                     "right": 0,
+                    "color": alpha(theme.palette.secondary.light, 0.01),
                 },
             },
-            "icon": {
-                "color": theme.palette.text.primary,
-                "fontSize": theme.spacing(2),
-            }
+            "fullscreen": {
+                "position": "absolute",
+                "bottom": "10px",
+                "right": "0",
+                "color": "red",
+            },
         };
     });
